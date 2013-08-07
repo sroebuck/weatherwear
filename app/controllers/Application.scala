854cf499eebdfa542617014d86a002ce00113f86
Stuart Roebuck <stuart.roebuck@gmail.com>
2013-08-08 00:44:02 +0100

    FIXME: Further progress.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,27 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.libs.WS
+import play.api.libs.json.Json
+import play.api.libs.json.JsValue
 
 object Application extends Controller {
   
+  val keyString = "1fd60563-da77-4bb9-88d5-0444be01310f"
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val responseFuture = WS.url("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist")
+      .setQueryParameter("key",keyString)
+      .get
+    val locationsJson = responseFuture.get.getBody
+    val json = Json.parse(locationsJson)
+    // val locations = (json \ "locations").as[List[JsValue]]
+    // val idPlaceMappings = locations.map {
+    //   location =>
+    //   (location \ "id").as[String] -> (location \ "name").as[String]
+    // }.toMap
+    // Ok(views.html.index(Json.toJson(idPlaceMappings).toString))
+    Ok(views.html.index(json.toString))
   }
   
 }
--- public/js/thecode.js
+++ public/js/thecode.js
@@ -1,14 +1,26 @@
-angular.module('weatherproof', ['ui.bootstrap']);
+var myApp = angular.module('weatherproof', ['ui.bootstrap', 'ngResource']);
+
+myApp.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+var keyString = '1fd60563-da77-4bb9-88d5-0444be01310f';
 
 function TypeaheadCtrl($scope, $http) {
     $scope.selected = undefined;
     $scope.states = ['Edinburgh', 'Glasgow', 'Aberdeen', 'Dundee'];
+    // var locationsResource = $resource('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist',
+    // 	{key: keyString}, {method: 'GET'});
     getLocationsList();
 
+
     function getLocationsList() {
-        var url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=1fd60563-da77-4bb9-88d5-0444be01310f';
-        $http.get(url, function(json) {
-            console.log(json);
+        var promise = $http.get('http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist', {
+        	params: {key: keyString}
+        });
+        promise.success(function(json) {
+        	console.log(json);
         });
     }
 
