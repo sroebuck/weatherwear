c2cfb5182cf65796c4b359203477ded5334cf231
rachelw1997 <rachelwalker22.1997@gmail.com>
2013-08-07 15:58:03 +0100

    Newest buttons

--- public/js/thecode.js
+++ public/js/thecode.js
@@ -1,5 +1,5 @@
 angular.module('weatherproof', ['ui.bootstrap']);
 function TypeaheadCtrl($scope) {
 	$scope.selected = undefined;
-	$scope.states = ['Edinburgh', 'Glasgow', 'Aberdeen', 'Dundee'];
+	$scope.states = ['Edinburgh', 'Glasgow', 'Aberdeen', 'Dundee','Birmingham','London','Manchester','Cardiff','Belfast','',];
 }
