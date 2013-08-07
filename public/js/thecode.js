8078e29488c3b81ffa62ac07965fad1ae90d300c
Stuart Roebuck <stuart.roebuck@gmail.com>
2013-08-07 22:07:28 +0100

    FIXME: Not working yet

--- public/js/thecode.js
+++ public/js/thecode.js
@@ -1,5 +1,17 @@
 angular.module('weatherproof', ['ui.bootstrap']);
-function TypeaheadCtrl($scope) {
-	$scope.selected = undefined;
-	$scope.states = ['Edinburgh', 'Glasgow', 'Aberdeen', 'Dundee'];
+
+function TypeaheadCtrl($scope, $http) {
+    $scope.selected = undefined;
+    $scope.states = ['Edinburgh', 'Glasgow', 'Aberdeen', 'Dundee'];
+    getLocationsList();
+
+    function getLocationsList() {
+        var url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=1fd60563-da77-4bb9-88d5-0444be01310f';
+        $http.get(url, function(json) {
+            console.log(json);
+        });
+    }
+
+
 }
+
