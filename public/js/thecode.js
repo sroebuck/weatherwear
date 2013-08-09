12cb5e82885560179b9dcc47447319f542ccbabd
Stuart Roebuck <stuart.roebuck@gmail.com>
2013-08-09 23:28:44 +0100

    Code now correctly downloading the weather for the selected location.

--- public/js/thecode.js
+++ public/js/thecode.js
@@ -1,12 +1,37 @@
 var myApp = angular.module('weatherproof', ['ui.bootstrap', 'ngResource']);
 
+// ECMA Script 5 style keys function definition...
+if (!Object.keys) Object.keys = function(o) {
+    if (o !== Object(o)) throw new TypeError('Object.keys called on non-object');
+    var ret = [];
+    var p;
+    for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+    return p;
+}
+
 function TypeaheadCtrl($scope, $http) {
-    $scope.selected = undefined;
+    $scope.selectedLocation = undefined;
 
     // Download the list of locations
     $http.get('/api/locations').success(function(json) {
-        $scope.states = json;
+        $scope.locationsMap = json;
+        $scope.locations = Object.keys($scope.locationsMap).sort()
     });
+
+
+    function changedLocation(newLocation, oldLocation, scope) {
+        var isValidLocation = _.contains($scope.locations, newLocation);
+        if (isValidLocation) {
+            var locationId = $scope.locationsMap[$scope.selectedLocation];
+            $http.get('/api/weather/' + locationId).success(function(json) {
+                $scope.weathers = json;
+                console.log($scope.weathers);
+            });
+
+        }
+    }
+
+    $scope.$watch('selectedLocation', changedLocation);
 
 }
 
@@ -23,5 +48,8 @@ function HoursOutdoors($scope) {
     }
 
     $scope.$watch('hour', changedHours, true);
+
+
+
 
 }
